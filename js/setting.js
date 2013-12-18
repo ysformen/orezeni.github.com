ad34066ce6cd79a197b53343f324f79ec94eaa1b
Itsuya Ikumi <ysformen@gmail.com>
2013-12-18 09:57:15 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -19,42 +19,61 @@ OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
   //議会費
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#935B3B' },
   //総務費
-  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
-  //総務管理費
-  '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2': { icon: 'icons/ekonomija.svg', color: '#C75746', bcolor: '#0AB971' },
+  //総務監理費
+  '2-1': { icon: 'icons/ekonomija.svg', color: '#C75746', bcolor: '#0AB971' },
   //徴税費
-  '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  //徴税費
-  '2-3': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  //徴税費
-  '2-4': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  //徴税費
-  '2-5': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  //徴税費
-  '2-6': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  //徴税費
-  '2-7': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2-2': { icon: 'icons/financial-admin.svg', color: '#C75746', bcolor: '#0AB971' },
+  //戸籍住民基本台帳費
+  '2-3': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#0AB971' },
+  //選挙費
+  '2-4': { icon: 'icons/vote.svg', color: '#C75746', bcolor: '#0AB971' },
+  //統計調査費
+  '2-5': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#0AB971' },
+  //監査委員費
+  '2-6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#0AB971' },
 
-  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-1': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '3-2': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
+  //民生費
+  '3': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#4E6D00' },
+  //社会福祉費
+  '3-1': { icon: 'icons/school-art-disabilities.svg', color: '#C75746', bcolor: '#4E6D00' },
+  //児童福祉費
+  '3-2': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#4E6D00' },
+  //生活保護費
   '3-3': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  //老人福祉費
+  '3-4': { icon: 'icons/old-age.svg', color: '#C75746', bcolor: '#4E6D00' },
+    //災害救助費
+  '3-5': { icon: 'icons/restoration.svg', color: '#C75746', bcolor: '#4E6D00' },
+
+  //衛生費
   '4': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
+  //
   '4-1': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
+  //
   '4-2': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
+  //
   '4-3': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
+  //
   '5': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
+  //
   '5-1': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  //
   '5-2': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  //
   '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
+  //
   '6-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
+  //
   '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
+  //
   '7-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
+  //
   '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
-  '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
+  //  '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
   '8-2': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
   '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
   '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
